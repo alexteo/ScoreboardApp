1d1fb2ac785ac0808becaba3ca648b34d3a1be67
alexteo <alex.teodor87@yahoo.com>
2019-01-22 20:41:20 +0200

    Updates

---
+++ precache-manifest.b3c1cf87b45fb42f2b7274ade0bc5f98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ce904b76a5ff0a475c7",
+    "url": "/ScoreboardApp/static/js/runtime~main.0ce904b7.js"
+  },
+  {
+    "revision": "69874688d57c118f948c",
+    "url": "/ScoreboardApp/static/js/main.69874688.chunk.js"
+  },
+  {
+    "revision": "fe65e5159218a8a08e5e",
+    "url": "/ScoreboardApp/static/js/1.fe65e515.chunk.js"
+  },
+  {
+    "revision": "69874688d57c118f948c",
+    "url": "/ScoreboardApp/static/css/main.a8a6b072.chunk.css"
+  },
+  {
+    "revision": "06d80ce13bedc581c86fb140d8507cc7",
+    "url": "/ScoreboardApp/index.html"
+  }
+];
